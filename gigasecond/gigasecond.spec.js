428380f29798bcd9272059b4c8a427ab05599b0a
programmiri <programmiri@gmail.com>
2019-03-22 20:50:39 +0100

    Solve exercise gigasecond

--- gigasecond/gigasecond.spec.js
+++ gigasecond/gigasecond.spec.js
@@ -7,13 +7,13 @@ describe('Gigasecond', () => {
     expect(gs).toEqual(expectedDate);
   });
 
-  xtest('tells the anniversary is next day when you are born at night', () => {
+  test('tells the anniversary is next day when you are born at night', () => {
     const gs = gigasecond(new Date(Date.UTC(2015, 8, 14, 23, 59, 59)));
     const expectedDate = new Date(Date.UTC(2047, 4, 24, 1, 46, 39));
     expect(gs).toEqual(expectedDate);
   });
 
-  xtest('even works before 1970 (beginning of Unix epoch)', () => {
+  test('even works before 1970 (beginning of Unix epoch)', () => {
     const gs = gigasecond(new Date(Date.UTC(1959, 6, 19, 5, 13, 45)));
     const expectedDate = new Date(Date.UTC(1991, 2, 27, 7, 0, 25));
     expect(gs).toEqual(expectedDate);
