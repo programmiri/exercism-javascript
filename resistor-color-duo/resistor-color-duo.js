d4d14b6ffe7d2c72a8f589df370801fb474797d9
programmiri <programmiri@gmail.com>
2019-07-04 22:48:26 +0200

    Add modified solution for resistor color duo

--- resistor-color-duo/resistor-color-duo.js
+++ resistor-color-duo/resistor-color-duo.js
@@ -11,14 +11,13 @@ const colorMap = [
   'white',
 ];
 
+function getColorCode(name) {
+  return colorMap.indexOf(name);
+}
+
 function value(colorNameList) {
-  const numAsString = colorNameList
-    .reduce((acc, curr) => {
-      acc.push(colorMap.indexOf(curr));
-      return acc;
-    }, [])
-    .join('');
-  return Number(numAsString);
+  const colorCodes = colorNameList.map(getColorCode).join('');
+  return Number(colorCodes);
 }
 
 export { value };
