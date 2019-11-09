835563fb6d21dbf49dd4f2b78ba8ff67b5d87a7b
programmiri <programmiri@gmail.com>
2019-11-09 18:06:09 +0100

    third iteration

--- word-count/word-count.js
+++ word-count/word-count.js
@@ -3,11 +3,11 @@ export class Words {
   	const words = string.toLowerCase().trim().split(/[\s]+/g)
 
   	return words.reduce((acc, curr) => {
-  		if (acc[curr] && !isNaN(acc[curr])) {
-  			acc[curr] = acc[curr] + 1
-  		} else {
+  		if (!acc[curr] || acc[curr] && isNaN(acc[curr])) {
 				acc[curr] = 1
+				return acc
   		}
+  		acc[curr] += 1
 			return acc
   	}, {})
   }
