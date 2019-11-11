4afb9445655fc075f80b264035cc16bae37201f2
programmiri <programmiri@gmail.com>
2019-11-11 22:01:44 +0100

    Add first solution for perfect number

--- perfect-numbers/perfect-numbers.js
+++ perfect-numbers/perfect-numbers.js
@@ -1,8 +1,33 @@
-//
-// This is only a SKELETON file for the 'Perfect Numbers' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
+function isNaturalNumber(num) {
+	return Number.isInteger(num) &&  num > 0 
+}
 
-export const classify = () => {
-  throw new Error("Remove this statement and implement this function");
+
+function sumOfDivisors(num) {
+	let result = 0;
+	for (let i = 1; i < num; i++) {
+		if (num % i ===0) {
+			result += i
+		}
+	}
+	return result 
+}
+
+export const classify = (num) => {
+	if(!isNaturalNumber(num)) {
+		throw new Error('Classification is only possible for natural numbers.')
+	}
+
+	const divisorSum = sumOfDivisors(num) 
+	if (divisorSum === num) {
+		return "perfect"
+	} 
+
+	if (divisorSum > num) {
+		return "abundant"
+	}	
+
+	if (divisorSum < num) {
+		return "deficient"
+	}
 };
