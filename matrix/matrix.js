bd514721ce331d85287ee68718e0ef6874c08854
programmiri <programmiri@gmail.com>
2020-03-15 21:30:59 +0100

    Refactore solution to task matrix

--- matrix/matrix.js
+++ matrix/matrix.js
@@ -1,31 +1,34 @@
 export class Matrix {
   constructor(string) {
-    this.matrix = string;
-    this.firstLineNumber = new RegExp(/^([0-9]+)/gm) 
-    this.leadingWhitespace = new RegExp(/^[ \t]+/gm)
+    this._rows = this.splitInRows(string)
+    this._columns = this.splitInColumns(string)
   }
 
-  splitInRows(stringMatrix) {
-    return stringMatrix.split('\n').map( entry => entry.split(' ').map(entry => parseInt(entry, 10)))
+  splitInRows(matrix) {
+    return matrix.split('\n').map( entry => entry.split(' ').map(entry => parseInt(entry, 10)))
   }
 
-  splitInColumns(stringMatrix) {
+  splitInColumns(matrix) {
+    const firstLineNumber = new RegExp(/^([0-9]+)/gm) 
+    const leadingWhitespace = new RegExp(/^[ \t]+/gm)
     const columns = []
 
-    let matrixToWorkOn = stringMatrix
+    let matrixToWorkOn = matrix
+
     while (matrixToWorkOn.trim().length) {
-      const column = matrixToWorkOn.match(this.firstLineNumber).map(entry => parseInt(entry, 10))
+      const column = matrixToWorkOn.match(firstLineNumber).map(entry => parseInt(entry, 10))
       columns.push([... column])
-      matrixToWorkOn = matrixToWorkOn.replace(this.firstLineNumber, '').replace(this.leadingWhitespace, '')
+
+      matrixToWorkOn = matrixToWorkOn.replace(firstLineNumber, '').replace(leadingWhitespace, '')
     }
    return columns
   }
 
   get rows() {
-    return this.splitInRows(this.matrix)    
+    return this._rows
   }
 
   get columns() {
-    return this.splitInColumns(this.matrix)
+    return this._columns
   }
 }
